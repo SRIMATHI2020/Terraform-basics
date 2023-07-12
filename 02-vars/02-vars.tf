d17e926860285622d457d06395ed8b97e747fd6c
Selvaraj <srimathi.radha@philips.com>
2023-07-12 19:54:06 +0530

    Auto Push

--- 02-vars/02-vars.tf
+++ 02-vars/02-vars.tf
@@ -3,16 +3,43 @@ variable "sample" {
 }
 
 output "sample_output" {
-    value = var.sample
+    default = var.sample
 }
-
- variable "sample_op" {
-        default = "Welcome to terraform training"
+variable "sample_op" {
+    default = "Welcome to Terraform training"
 }
-
-### variable can be called as var.variableName. but if access between set of strings it should be in quotes 
-### In terraform , no singlr quotes
 
 output "sample_var_op" {
     value = " Value of the variable is ${var.sample_op}"
+}
+
+variable "example_list" {   
+    default = [
+        "AWS",
+        "DevOps",
+         55,
+        "Trainer" ,
+        "Terraform"
+    ]
+}
+
+# Try to understand here, the data in the list doesn't necessary to be of same data type. 
+
+output "example_list_op" {
+    value = "Welcome to ${var.example_list[1]}  with ${var.example_list[0]} Training and this is our batch ${var.example_list[2]} and the current topic is ${var.example_list[4]}"
+}
+
+
+# What is a map variable ? A variable with mutiple key value pairs is referred as Map Varaible
+
+variable "example_map" {
+    default = {
+        Batch    = "B55"
+        Training = "DevOpswithAWS"
+        Mode     = "Online"
+    }
+}
+
+output "example_map_op" {
+    value = "We are learning ${var.example_map["Training"]} and this is our ${var.example_map["Batch"]} and the it is ${var.example_map["Mode"]} "
 }
