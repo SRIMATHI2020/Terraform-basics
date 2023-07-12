7a29abdb36ebe4d311dc9276b023a5e32d6131a1
Selvaraj <srimathi.radha@philips.com>
2023-07-12 19:12:20 +0530

    Auto Push

--- 02-vars/02-vars.tf
+++ 02-vars/02-vars.tf
@@ -2,13 +2,13 @@ variable "sample" {
     default = "Welcome to Terraform"
 }
 
-output "sample-op" {
+output "sample_output" {
     value = var.sample
 }
 
 ### variable can be called as var.variableName. but if access between set of strings it should be in quotes 
 ### In terraform , no singlr quotes
 
-output "sample-var-op" {
-    value = " Value of the variable is ${var.sample-op}"
+output "sample_var_op" {
+    value = " Value of the variable is ${var.sample_op}"
 }
