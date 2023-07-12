2c3b0624db1616b75601c30aa86aa9978f563fe5
Selvaraj <srimathi.radha@philips.com>
2023-07-12 20:22:38 +0530

    Auto Push

---
+++ 03-vars/sample.tf
@@ -0,0 +1,14 @@
+# Declaring an empty variable city 
+
+variable "city" {}
+
+output "city_name" {
+    value = "Our city name is ${var.city}"
+}
+
+
+variable "state" {}
+
+output "state_name" {
+    value  = "Out State name is ${var.state}"
+}
