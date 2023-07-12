165c745df7eaded994f8f64fae558210711cf897
Selvaraj <srimathi.radha@philips.com>
2023-07-12 17:20:05 +0530

    Auto Push

--- 02-vars/02-vars.tf
+++ 02-vars/02-vars.tf
@@ -5,3 +5,10 @@ variable "sample" {
 output "sample-op" {
     value = var.sample
 }
+
+### variable can be called as var.variableName. but if access between set of strings it should be in quotes 
+### In terraform , no singlr quotes
+
+output "sample-var-op"{
+    value = " value of the variable is ${var.sample-op}"
+}
