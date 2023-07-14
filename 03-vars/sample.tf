3c81fa75562bac9277acb100e2248bf43a6c3435
Selvaraj <srimathi.radha@philips.com>
2023-07-14 11:27:12 +0530

    Auto Push

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -11,4 +11,11 @@ variable "state" {}
 
 output "state_name" {
     value  = "Our State name is ${var.state}"
+
+variable "cold_city" {}
+
+output "city_name" {
+    value = "Value of the cold city is ${var.city_name}"
+}
+
 }
