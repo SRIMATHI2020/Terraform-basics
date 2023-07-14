4a392c8194d00ece5647d415b2bfb1d9430d70a0
Selvaraj <srimathi.radha@philips.com>
2023-07-14 12:54:55 +0530

    Auto Push

--- example/ec2.tf
+++ example/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = "ami-05d4acaaa6c564dc7"
+  ami           = "ami-0b1992df957b7cf82"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "TerraformServer"
+    Name = "HelloWorld"
   }
 }
--- example/provider.tf
+++ example/provider.tf
@@ -1,2 +1,3 @@
 provider "aws" {
+  # Configuration options
 }
